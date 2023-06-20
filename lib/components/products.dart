c436b277f8f8a147fb187f54248aaa2e7f3876ed
Anshul <anshulkapoor306@gmail.com>
2023-06-21 00:35:14 +0530

    image add

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Product extends StatefulWidget {
   const Product({Key? key}) : super(key: key);
 
@@ -7,8 +8,76 @@ class Product extends StatefulWidget {
 }
 
 class _ProductState extends State<Product> {
+  var product_list = [
+    {
+      "name": "Sports_tighty",
+      "picture": "images/icons/bags.png",
+      "old_price": "560",
+      "price": "450",
+    },
+    {
+      "name": "pants",
+      "picture": "images/p1.jpg",
+      "old_price": "980",
+      "price": "730",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.builder(
+        itemCount: product_list.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return Single_prod(
+            prod_name: product_list[index]['name'],
+            prod_old_price: product_list[index]['old_price'],
+            prod_picture: product_list[index]['image'],
+            prod_price: product_list[index]['price'],
+          );
+        });
+  }
+}
+
+class Single_prod extends StatelessWidget {
+  final prod_name;
+  final  prod_picture;
+  final prod_old_price;
+  final prod_price;
+  Single_prod({
+    this.prod_name,
+    this.prod_old_price,
+    this.prod_picture,
+    this.prod_price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Hero(
+        tag: prod_name,
+        child: Material(
+          child: InkWell(
+            onTap: () {},
+            child: GridTile(
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    prod_name,
+                    style: TextStyle(fontWeight: FontWeight.w800),
+                  ),
+                ),
+              ),
+             child: Image.asset(
+              prod_picture != null ? prod_picture : "Hello",
+              // prod_picture,
+               fit: BoxFit.cover,
+             ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
